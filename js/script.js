eb6cd6da598b5b67a1260249e3b4c6d1927d6717
Sergey <mons198x@gmail.com>
2021-04-21 01:22:13 +0300

    import videobase

---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const gloAcademyList = document.querySelector('.glo-academy-list');
+
+    const createCard = (dataVideo) => {
+        const imgUrl = dataVideo.snippet.thumbnails.high.url,
+            videoId = dataVideo.id.videoId,
+            title = dataVideo.snippet.title,
+            dateVideo = dataVideo.snippet.publishedAt,
+            channelTitle = dataVideo.snippet.channelTitle;
+
+        const card = document.createElement('div');
+        card.classList.add('video-card');
+        card.innerHTML = `
+            <div class="video-thumb">
+                <a class="link-video youtube-modal" href="https://www.youtube.com/watch?v=${videoId}">
+                    <img src=${imgUrl} alt="" class="thumbnail">
+                </a>
+            </div>
+            <h3 class="video-title">${title}</h3>
+            <div class="video-info">
+                <span class="video-counter">
+                    <span class="video-date">${dateVideo}</span>
+                </span>
+                <span class="video-channel">${channelTitle}</span>
+            </div>
+        `;  
+        return card;
+    };
+
+    const createList = (wrapper, listVideo) => {
+        wrapper.textContent = '';
+        
+        listVideo.forEach(item => {
+            console.log(item);
+            wrapper.append(createCard(item));
+        });
+    };
+
+    createList(gloAcademyList, gloAcademy);
+});
